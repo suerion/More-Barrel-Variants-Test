fbe0a77cca3768223c116a1dee0aea006af427e2
pnk2u <159317511+pnk2u@users.noreply.github.com>
2024-06-16 20:55:37 +0200

    Rename the abbreviation from "mbv" to "mblv"

---
+++ src/main/java/com/pnku/mblv/MoreBarrelVariants.java
@@ -0,0 +1,22 @@
+package com.pnku.mblv;
+
+import com.pnku.mblv.init.MblvBlockInit;
+import com.pnku.mblv.init.MblvItemInit;
+import com.pnku.mblv.poi.MblvPointOfInterestTypes;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+
+public class MoreBarrelVariants implements ModInitializer {
+    public static final String MODID = "lolmblv";
+
+    @Override
+    public void onInitialize() {
+        MblvBlockInit.registerBlocks();
+        MblvItemInit.registerItems();
+        MblvPointOfInterestTypes.init();
+    }
+
+    public static Identifier asId(String path) {
+        return Identifier.of(MODID, path);
+    }
+}
