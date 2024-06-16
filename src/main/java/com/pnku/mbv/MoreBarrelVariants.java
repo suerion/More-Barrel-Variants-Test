f988ca2c3a34bf6b87858aba24c7ab97f95b374c
pnk2u <159317511+pnk2u@users.noreply.github.com>
2024-06-16 18:40:23 +0200

    Initial Commit 1.21
    Lots of data folder renames (plural to singular)

--- src/main/java/com/pnku/mbv/MoreBarrelVariants.java
+++ src/main/java/com/pnku/mbv/MoreBarrelVariants.java
@@ -17,6 +17,6 @@ public class MoreBarrelVariants implements ModInitializer {
     }
 
     public static Identifier asId(String path) {
-        return new Identifier(MODID, path);
+        return Identifier.of(MODID, path);
     }
 }
